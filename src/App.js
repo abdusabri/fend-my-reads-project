9fe0a51b208e774748216813adcc5455d3290725
Abdu Assabri <abdusabri@outlook.com>
2018-10-03 10:36:41 +0200

    Create BookSearch component, implement navigation

--- src/App.js
+++ src/App.js
@@ -1,51 +1,46 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import { BrowserRouter, Route, Redirect, Switch, Link } from 'react-router-dom'
 import Shelves from './models/Shelves'
 import BookShelf from './components/BookShelf'
 import BookSearch from './components/BookSearch'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     shelves: Shelves.shelves
   }
 
   render() {
     return (
       <div className='app'>
-        {this.state.showSearchPage ? (
-          <div className='search-books'>
-            <BookSearch/>
-            <div className='search-books-results'>
-              <ol className='books-grid'></ol>
-            </div>
-          </div>
-        ) : (
-          <div className='list-books'>
-            <div className='list-books-title'>
-              <h1>MyReads</h1>
-            </div>
-            <div className='list-books-content'>
-              <div>
-              {this.state.shelves.map((shelf) => (
-                <BookShelf 
-                  key={shelf.id}
-                  title={shelf.title}/>
-              ))}
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' render={() => (
+              <div className='list-books'>
+                <div className='list-books-title'>
+                  <h1>MyReads</h1>
+                </div>
+                <div className='list-books-content'>
+                  <div>
+                    {this.state.shelves.map((shelf) => (
+                      <BookShelf
+                        key={shelf.id}
+                        title={shelf.title} />
+                    ))}
+                  </div>
+                </div>
+                <div className='open-search'>
+                  <Link to='/search'>Add a book</Link>
+                </div>
               </div>
-            </div>
-            <div className='open-search'>
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
+            )} />
+            <Route path='/search' render={() => (
+                <BookSearch />
+            )} />
+            <Redirect from='*' to='/' />
+          </Switch>
+        </BrowserRouter>
       </div>
     )
   }
