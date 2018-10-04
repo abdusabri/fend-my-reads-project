b4c6a0b11664fc2b03fd027711919cf70c153cdf
Abdu Assabri <abdusabri@outlook.com>
2018-10-04 22:45:57 +0200

    Get books from server and remove mock

--- src/App.js
+++ src/App.js
@@ -13,39 +13,21 @@ class BooksApp extends React.Component {
   }
 
   componentDidMount() {
-    //TODO: Remove mock
-    const books = [
-      {
-        id: '1',
-        title: "Ender's Game",
-        coverImageUrl: 'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api',
-        authors: [
-          'Orson',
-          'Scott',
-          'Card'
-        ],
-        shelf: 'currentlyReading'
-      },
-      {
-        id: '2',
-        title: "To Kill a Mockingbird",
-        coverImageUrl: 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api',
-        authors: [
-          'Harper',
-          'Lee'
-        ],
-        shelf: 'read'
-      }
-    ]
-    this.setState({ books : books})
-
     BooksAPI.getAll()
-      .then((res) => console.log(res))
+      .then((res) => {
+          const books = res.map((book) => ({
+            id: book.id,
+            title: book.title,
+            coverImageUrl: book.imageLinks.smallThumbnail,
+            authors: book.authors,
+            shelf: book.shelf
+          }))
+          this.setState({books})
+      })
       .catch((err) => console.log(err))
   }
 
   handleMoveBookToShelf = (bookId, shelf) =>{
-    // console.log(`AppComp. - BookId: "${bookId}" needs to be moved to "${shelf}" Shelf`)
     const booksCopy = [...this.state.books]
     const bookIndex = booksCopy.findIndex((b) => b.id === bookId)
     booksCopy[bookIndex].shelf = shelf
