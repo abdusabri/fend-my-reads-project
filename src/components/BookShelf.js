62acc5c483e4831a46347c72ef599228da198e28
Abdu Assabri <abdusabri@outlook.com>
2018-10-08 19:49:33 +0200

    Convert BookShelf to a stateless component

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Book from './Book'
 
-class BookShelf extends Component {
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        books: PropTypes.array.isRequired,
-        onMoveBookToShelf: PropTypes.func.isRequired
+const BookShelf = (props) => {
+    
+    const handleMoveBookToShelf = (bookId, shelf) =>{
+        props.onMoveBookToShelf(bookId, shelf)
     }
 
-    handleMoveBookToShelf = (bookId, shelf) =>{
-        this.props.onMoveBookToShelf(bookId, shelf)
-    }
-
-    render () {
-        return (
-            <div className='bookshelf'>
-                <h2 className='bookshelf-title'>{this.props.title}</h2>
-                <div className='bookshelf-books'>
-                    <ol className='books-grid'>
-                        {this.props.books.map((book) => (
-                            <Book
-                                key={book.id}
-                                id={book.id}
-                                title={book.title}
-                                coverImageUrl={book.coverImageUrl}
-                                authors={book.authors}
-                                shelf={book.shelf}
-                                onMoveBookToShelf={this.handleMoveBookToShelf}/>
-                        ))}
-                    </ol>
-                </div>
+    return (
+        <div className='bookshelf'>
+            <h2 className='bookshelf-title'>{props.title}</h2>
+            <div className='bookshelf-books'>
+                <ol className='books-grid'>
+                    {props.books.map((book) => (
+                        <Book
+                            key={book.id}
+                            id={book.id}
+                            title={book.title}
+                            coverImageUrl={book.coverImageUrl}
+                            authors={book.authors}
+                            shelf={book.shelf}
+                            onMoveBookToShelf={handleMoveBookToShelf}/>
+                    ))}
+                </ol>
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+BookShelf.propTypes = {
+    title: PropTypes.string.isRequired,
+    books: PropTypes.array.isRequired,
+    onMoveBookToShelf: PropTypes.func.isRequired
+}
+
 
 export default BookShelf
