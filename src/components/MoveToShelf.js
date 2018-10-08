9943e6cc42b7e581179d1fe0a9cc96a78a7eac6d
Abdu Assabri <abdusabri@outlook.com>
2018-10-08 19:43:30 +0200

    Convert MoveToShelf to a stateless component

--- src/components/MoveToShelf.js
+++ src/components/MoveToShelf.js
@@ -1,32 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Shelves from '../models/Shelves'
 
-class MoveToShelf extends Component {
-    static propTypes = {
-        bookShelf: PropTypes.string.isRequired,
-        onMoveBookToShelf: PropTypes.func.isRequired
+const MoveToShelf = (props) => {
+    
+    const handleChangeEvent = (e) => {
+        props.onMoveBookToShelf(e.target.value)
     }
 
-    handleChangeEvent = (e) => {
-        this.props.onMoveBookToShelf(e.target.value)
-    }
+    return (
+        <div className='book-shelf-changer'>
+            <select defaultValue={props.bookShelf}
+                onChange={handleChangeEvent}>
+                    <option value='move' disabled>Move to...</option>
+                    {Shelves.shelves.map((shelf) => (
+                        <option key={shelf.id} value={shelf.value}>
+                            {shelf.title}</option>
+                    ))}
+                    <option value='none'>None</option>
+            </select>
+        </div>
+    )
+}
 
-    render () {
-        return (
-            <div className='book-shelf-changer'>
-                <select defaultValue={this.props.bookShelf}
-                    onChange={this.handleChangeEvent}>
-                        <option value='move' disabled>Move to...</option>
-                        {Shelves.shelves.map((shelf) => (
-                            <option key={shelf.id} value={shelf.value}>
-                                {shelf.title}</option>
-                        ))}
-                        <option value='none'>None</option>
-                </select>
-            </div>
-        );
-    }
+MoveToShelf.propTypes = {
+    bookShelf: PropTypes.string.isRequired,
+    onMoveBookToShelf: PropTypes.func.isRequired
 }
 
 export default MoveToShelf
