81c7207b3a2b8b0ff421322dc60815354f0c275d
Abdu Assabri <abdusabri@outlook.com>
2018-10-08 19:46:25 +0200

    Convert MyReads to a stateless component

--- src/components/MyReads.js
+++ src/components/MyReads.js
@@ -1,42 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import BookShelf from './BookShelf'
 import { Link } from 'react-router-dom'
 
-class MyReads extends Component {
-    static propTypes = {
-        shelves: PropTypes.array.isRequired,
-        books: PropTypes.array.isRequired,
-        onMoveBookToShelf: PropTypes.func.isRequired
+const MyReads = (props) => {
+
+    const handleMoveBookToShelf = (bookId, shelf) =>{
+        props.onMoveBookToShelf(bookId, shelf)
     }
 
-    handleMoveBookToShelf = (bookId, shelf) =>{
-        this.props.onMoveBookToShelf(bookId, shelf)
-    }
+    return (
+        <div className='list-books'>
+            <div className='list-books-title'>
+                <h1>MyReads</h1>
+            </div>
+            <div className='list-books-content'>
+                <div>
+                    {props.shelves.map((shelf) => (
+                        <BookShelf
+                            key={shelf.id}
+                            title={shelf.title}
+                            books={props.books.filter((book) => book.shelf === shelf.value)}
+                            onMoveBookToShelf={handleMoveBookToShelf}/>
+                    ))}
+                </div>
+            </div>
+            <div className='open-search'>
+                <Link to='/search'>Add a book</Link>
+            </div>
+        </div>             
+    )   
+}
 
-    render () {
-        return (
-            <div className='list-books'>
-                <div className='list-books-title'>
-                    <h1>MyReads</h1>
-                </div>
-                <div className='list-books-content'>
-                    <div>
-                        {this.props.shelves.map((shelf) => (
-                            <BookShelf
-                                key={shelf.id}
-                                title={shelf.title}
-                                books={this.props.books.filter((book) => book.shelf === shelf.value)}
-                                onMoveBookToShelf={this.handleMoveBookToShelf}/>
-                        ))}
-                    </div>
-                </div>
-                <div className='open-search'>
-                    <Link to='/search'>Add a book</Link>
-                </div>
-            </div>             
-        )
-    }    
+MyReads.propTypes = {
+    shelves: PropTypes.array.isRequired,
+    books: PropTypes.array.isRequired,
+    onMoveBookToShelf: PropTypes.func.isRequired
 }
 
 export default MyReads
